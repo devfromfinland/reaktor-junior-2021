f85297228019fb48673a551b0cd80895af178aef
devfromfinland <contact@vietphan.info>
2020-11-08 22:18:34 +0200

    move data fetching to parent App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import CategoryPage from './CategoryPage'
+import axios from 'axios'
 import './App.css'
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/jackets'>Jackets</Link>
-            </li>
-            <li>
-              <Link to='/shirts'>Shirts</Link>
-            </li>
-            <li>
-              <Link to='/accessories'>Accessories</Link>
-            </li>
-          </ul>
-        </nav>
-        
-        <Switch>
-          <Route path='/' exact render={() => (<div>Hello world</div>)}/>
-          <Route path='/:category' component={CategoryPage} />
-          <Route path='*' render={() => (<div>404</div>)} />
-        </Switch>
-      </div>
-    </Router>
-  )
+  const [ jackets, setJackets ] = useState(null)
+  const [ shirts, setShirts ] = useState(null)
+  const [ accessories, setAccessories ] = useState(null)
+  const [ isLoaded, setIsLoaded ] = useState(false)
+
+  useEffect(() => {
+    fetchAllData()
+
+    return () => {
+      // cancel API query if necessary
+    }
+  }, [])
+
+  const fetchAllData = async () => {
+    const res1 = await axios.get('https://bad-api-assignment.reaktor.com/products/jackets')
+    const res2 = await axios.get('https://bad-api-assignment.reaktor.com/products/shirts')
+    const res3 = await axios.get('https://bad-api-assignment.reaktor.com/products/accessories')
+    setIsLoaded(true)
+    setJackets(res1.data)
+    setShirts(res2.data)
+    setAccessories(res3.data)
+    // jackets: 7686 items ~ 1.3 MB memory
+    // shirts: 6815 items ~ 1.2 MB memory
+    // accessories: 5381 items ~ 1 MB memory
+    // 3 MB is not too bad for saving in memory
+  }
+
+  if (!isLoaded) {
+    return (
+    <div>Page is loading...</div>
+  )} else {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/jackets'>Jackets</Link>
+              </li>
+              <li>
+                <Link to='/shirts'>Shirts</Link>
+              </li>
+              <li>
+                <Link to='/accessories'>Accessories</Link>
+              </li>
+            </ul>
+          </nav>
+          
+          <Switch>
+            <Route path='/' exact render={() => (<div>Hello world</div>)}/>
+            <Route path='/:category' render={({ match }) => {
+              switch (match.params.category) {
+                case 'jackets':
+                  return <CategoryPage data={jackets}/>
+                case 'shirts':
+                  return <CategoryPage data={shirts}/>
+                case 'accessories':
+                  return <CategoryPage data={accessories}/>
+                default:
+                  return <CategoryPage />
+              }
+            }}/>
+            <Route path='*' render={() => (<div>404</div>)} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App
--- src/CategoryPage/index.js
+++ src/CategoryPage/index.js
@@ -1,40 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
+import React from 'react'
 import Item from './Item'
-// import { checkMemSize } from '../utils/helpers'
 
-const CategoryPage = () => {
-  const [ data, setData ] = useState(null)
-  const { category } = useParams()
-
-  useEffect(() => {
-    axios.get(`https://bad-api-assignment.reaktor.com/products/${category}`, {
-      headers: {
-        'x-force-error-mode': 'all' // force the failure to reproduce
-      }
-    })
-      .then(response => {
-        // let memSize = checkMemSize(response.data)
-        // console.log(`mem size: ${memSize} bytes`)
-        setData(response.data)
-      })
-  }, [category])
-
-  // jackets: 7686 items ~ 1.3 MB memory
-  // shirts: 6815 items ~ 1.2 MB memory
-  // accessories: 5381 items ~ 1 MB memory
-
+const CategoryPage = ({ data }) => {
   return (
     <div>
-      Category: { category }
       { data
         ? <ul>
           {/* quite a long list to render */}
-          <Item item={data[0]} />
-          {/* { data.map(item => <Item key={item.id} item={item}/>)} */}
+          { data.map(item => <Item key={item.id} item={item}/>)}
         </ul>
-        : 'Loading data...'}
+        : 'No data to show'}
     </div>
   )
 }

ee3d47b6c0547339601c95304d41329f84bdda3d
devfromfinland <contact@vietphan.info>
2020-11-09 12:38:29 +0200

    refactor data fetching

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import CategoryPage from './CategoryPage'
-import axios from 'axios'
+import { getProductsByCategory } from './services/categoryService'
 import './App.css'
 
 const App = () => {
   const [ jackets, setJackets ] = useState(null)
   const [ shirts, setShirts ] = useState(null)
   const [ accessories, setAccessories ] = useState(null)
-  const [ isLoaded, setIsLoaded ] = useState(false)
 
   useEffect(() => {
     fetchAllData()
@@ -19,63 +18,55 @@ const App = () => {
   }, [])
 
   const fetchAllData = async () => {
-    const res1 = await axios.get('https://bad-api-assignment.reaktor.com/products/jackets')
-    const res2 = await axios.get('https://bad-api-assignment.reaktor.com/products/shirts')
-    const res3 = await axios.get('https://bad-api-assignment.reaktor.com/products/accessories')
-    setIsLoaded(true)
-    setJackets(res1.data)
-    setShirts(res2.data)
-    setAccessories(res3.data)
+    setJackets(await getProductsByCategory('jackets'))
+    setShirts(await getProductsByCategory('shirts'))
+    setAccessories(await getProductsByCategory('accessories'))
+  
     // jackets: 7686 items ~ 1.3 MB memory
     // shirts: 6815 items ~ 1.2 MB memory
     // accessories: 5381 items ~ 1 MB memory
-    // 3 MB is not too bad for saving in memory
+    // 3 MB is not too bad for saving them all in memory
   }
 
-  if (!isLoaded) {
-    return (
-    <div>Page is loading...</div>
-  )} else {
-    return (
-      <Router>
-        <div style={{ height: '100%' }}>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/jackets'>Jackets</Link>
-              </li>
-              <li>
-                <Link to='/shirts'>Shirts</Link>
-              </li>
-              <li>
-                <Link to='/accessories'>Accessories</Link>
-              </li>
-            </ul>
-          </nav>
-          
-          <Switch>
-            <Route path='/' exact render={() => (<div>Hello world</div>)}/>
-            <Route path='/:category' render={({ match }) => {
-              switch (match.params.category) {
-                case 'jackets':
-                  return <CategoryPage data={jackets}/>
-                case 'shirts':
-                  return <CategoryPage data={shirts}/>
-                case 'accessories':
-                  return <CategoryPage data={accessories}/>
-                default:
-                  return <CategoryPage />
-              }
-            }}/>
-            <Route path='*' render={() => (<div>404</div>)} />
-          </Switch>
-        </div>
-      </Router>
-    )
-  }
+  return (
+    <Router>
+      <div style={{ height: '100%' }}>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/jackets'>Jackets</Link>
+            </li>
+            <li>
+              <Link to='/shirts'>Shirts</Link>
+            </li>
+            <li>
+              <Link to='/accessories'>Accessories</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        <Switch>
+          <Route path='/' exact render={() => (<div>Hello world</div>)}/>
+          <Route path='/:category' render={({ match }) => {
+            switch (match.params.category) {
+              case 'jackets':
+                return <CategoryPage data={jackets}/>
+              case 'shirts':
+                return <CategoryPage data={shirts}/>
+              case 'accessories':
+                return <CategoryPage data={accessories}/>
+              default:
+                return <CategoryPage />
+            }
+          }}/>
+          <Route path='*' render={() => (<div>404</div>)} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -3,5 +3,16 @@ import axios from 'axios'
 const apiUrl = 'https://bad-api-assignment.reaktor.com'
 
 export const getProductsByCategory = async (category) => {
-  // placeholder
+  try {
+    const res = await axios.get(`${apiUrl}/products/${category}`, {
+      headers: {
+        'x-force-error-mode': 'all'
+      }
+    })
+
+    return res.data
+  } catch (err) {
+    // todo: handle err
+    console.log('error', err)
+  }
 }
